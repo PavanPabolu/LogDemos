b1c30fb6ee788b9d1d5beb0c5f1f8f9e0f58c344
PavanPabolu <PavanPabolu.edu@gmail.com>
2024-07-01 23:18:40 +0530

    ref link

--- Log.ExceptionHandling.Globally.WebAPI/Program.cs
+++ Log.ExceptionHandling.Globally.WebAPI/Program.cs
@@ -32,6 +32,33 @@ app.UseExceptionHandler(options =>
 });
 //***********************************************************************************
 
+/*//https://learn.microsoft.com/en-us/aspnet/core/fundamentals/error-handling?view=aspnetcore-8.0#exception-handler-page
+ app.UseExceptionHandler(exceptionHandlerApp =>
+    {
+        exceptionHandlerApp.Run(async context =>
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            // using static System.Net.Mime.MediaTypeNames;
+            context.Response.ContentType = Text.Plain;
+
+            await context.Response.WriteAsync("An exception was thrown.");
+
+            var exceptionHandlerPathFeature =
+                context.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+            {
+                await context.Response.WriteAsync(" The file was not found.");
+            }
+
+            if (exceptionHandlerPathFeature?.Path == "/")
+            {
+                await context.Response.WriteAsync(" Page: Home.");
+            }
+        });
+    });
+ */
 
 
 
